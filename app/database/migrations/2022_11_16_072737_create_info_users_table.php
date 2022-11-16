cd77bd98b4699cd394dbb3a98ff2638fc73eb488
maitien11 <65032709+maivantien1107@users.noreply.github.com>
2022-11-16 13:37:40 +0700

    add database

--- app/database/migrations/2022_11_16_072737_create_info_users_table.php
+++ app/database/migrations/2022_11_16_072737_create_info_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('info_users', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('user_id');
+            $table->string('avatar');
             $table->date('birthday')->nullable();
             $table->string('description')->nullable();
             $table->string('city');
