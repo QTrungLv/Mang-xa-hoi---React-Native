d93f2cd4e712303ecf0301b2a9a51d075d6a3052
maitien11 <65032709+maivantien1107@users.noreply.github.com>
2022-11-25 23:02:16 +0700

    add heroku

--- app/app/Models/User.php
+++ app/app/Models/User.php
@@ -22,11 +22,12 @@ class User extends Authenticatable implements JWTSubject
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
         'email',
         'password',
         'phone',
-        'username',
+        'sex',
+        'first_name',
+        'last_name',
         'is_verified',
         'email_verified_at',
         'phone_verified_at'
